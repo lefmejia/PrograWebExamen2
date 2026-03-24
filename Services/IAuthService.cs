3331f4a5e7db4863f7a9e1a9ccdc9eb4df4e8b4b
LuisSoundMachine <luis@sound-machine.com>
2026-03-23 20:41:34 -0600

    Funcion de registro

---
+++ Services/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using PlataformJuegoTorneo.Models;
+using PlataformJuegoTorneo.DTOs;
+
+namespace PlataformJuegoTorneo.Services
+{
+    public interface IAuthService
+    {
+        Task<Jugadores> Registro(RegistroDto registroDto);
+
+        Task<(Jugadores jugador, string token)> Login(LoginDto loginDto);
+
+        Task<bool> ValidateToken(string token);
+
+        Task<Jugadores?> GetJugadorById(string userId);
+
+        string GenerateJwtToken(Jugadores jugador);
+
+        Task<List<Jugadores>?> GetAllJugadores();
+    }
+}
