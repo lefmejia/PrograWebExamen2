5d39b4b3dda266ad912c5822a4eeeaf655b67eed
LuisSoundMachine <luis@sound-machine.com>
2026-03-23 20:18:46 -0600

    Model y DTOs de jugadores

---
+++ Models/Jugadores.cs
@@ -0,0 +1,33 @@
+﻿namespace PlataformJuegoTorneo.Models
+{
+    public class Jugadores
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Apellido { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string NombreUsuario { get; set; } = string.Empty;
+
+        public DateTime FechaRegistro { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Pais { get; set; } = string.Empty;
+
+        public string Rol { get; set; } = string.Empty;
+
+        public int PuntosGlobales { get; set; }
+
+        public int PuntosGanados { get; set; }
+
+        public DateTime UltimaConexion { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        public bool Conectado { get; set; } = false;
+    }
+}
